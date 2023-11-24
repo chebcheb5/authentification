bf7a5b425e678da2385f80eff0b42c2638a9ff5c
Asus <chalbichadha5@gmail.com>
2023-11-24 19:00:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetformation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projetformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in)
+    }
+}
